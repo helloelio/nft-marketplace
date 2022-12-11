7ecbc3c4f0c3848fa5a84c0660d719d728f2bddd
Медведев Глеб Медведев <g.medvedev@atol.ru>
2022-12-11 17:40:22 +0300

    F-v001 - setup button UI component

--- UI/Icons/index.tsx
+++ UI/Icons/index.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image';
 import React from 'react';
 import Plus from './Plus.svg';
+import ArrowRihtCircle from './arrow-right.svg';
 
 type IconProps = {
-  icon: any;
+  icon: 'plus | arrowRight';
   margin: any;
 };
 
 const icons: any = {
   plus: { icon: Plus, alt: 'plus icon' },
+  arrowRight: { icon: ArrowRihtCircle, alt: 'arrow right circle' },
 };
 
 const Icons = ({ icon, margin }: IconProps) => {
